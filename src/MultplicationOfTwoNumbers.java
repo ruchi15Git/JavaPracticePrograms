2a412f4d74dd79a3525a9ed2f8a08cb802695741
Anjali <anjuchate15@gmail.com>
2025-01-09 10:17:23 +0530

    multiplication

--- src/MultplicationOfTwoNumbers.java
+++ src/MultplicationOfTwoNumbers.java
@@ -2,6 +2,6 @@ public class MultplicationOfTwoNumbers {
     public static void main(String[] args){
         int num1=20, num2=8, result=0;
         result=num1*num2;
-        System.out.println("The multiplication of two numners is: "+result);
+        System.out.println("The multiplication of two numbers is: "+result);
     }
 }
