4ee4fa1a85cfd0ecca90a0b91c65033673b14498
Anjali <anjuchate15@gmail.com>
2025-01-09 07:01:22 +0530

    Pushing 2nd code

---
+++ src/AdditionOfTwoNumbers.java
@@ -0,0 +1,8 @@
+public class AdditionOfTwoNumbers {
+    public static void main(String[] args){
+        int x=10;
+        int y=20;
+        int sum = x+y;
+        System.out.println("The sum of two numbers x & y is: "+sum);
+    }
+}
