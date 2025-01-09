77f430a4ae145a991ce0ea996342309c78fd7ab9
Anjali <anjuchate15@gmail.com>
2025-01-09 07:16:16 +0530

    Find the large number among three nums

---
+++ src/FindGreatestNumberAmongThreeNum.java
@@ -0,0 +1,12 @@
+public class FindGreatestNumberAmongThreeNum {
+    public static void main(String[] args){
+        int a=1000, b=20, c=180;
+        if((a>b) && (a>c)){
+            System.out.println("The greatest number is: " +a);
+        } else if ((b>a) && (b>c)) {
+            System.out.println("The greatest number is: " +b);
+        }else{
+            System.out.println("The greatest number is :" +c);
+        }
+    }
+}
