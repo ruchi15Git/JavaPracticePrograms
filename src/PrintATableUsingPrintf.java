0e51e4b6a82ee4e88b8c76a3a16b845befe09cd7
Anjali <anjuchate15@gmail.com>
2025-01-12 00:55:12 +0530

    table of nine

--- src/PrintATableUsingPrintf.java
+++ src/PrintATableUsingPrintf.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class PrintATableUsingPrintf {
+    public static void main(String[] args){
+        System.out.println("The table of nine is as follows: ");
+        System.out.printf("%d\n",9);
+        System.out.printf("%d\n",18);
+        System.out.printf("%d\n",27);
+        System.out.printf("%d\n",36);
+        System.out.printf("%d\n",45);
+        System.out.printf("%d\n",54);
+        System.out.printf("%d\n",63);
+        System.out.printf("%d\n",72);
+        System.out.printf("%d\n",81);
+        System.out.printf("%d",90);
+
+    }
 }
