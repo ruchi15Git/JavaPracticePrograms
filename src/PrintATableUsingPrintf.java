295c899614c35680b9150391b3034f279775f783
Anjali <anjuchate15@gmail.com>
2025-01-17 06:32:08 +0530

    table for 9

--- src/PrintATableUsingPrintf.java
+++ src/PrintATableUsingPrintf.java
@@ -1,6 +1,6 @@
 public class PrintATableUsingPrintf {
     public static void main(String[] args){
-        System.out.println("The table of nine is as follows: ");
+        /*System.out.println("The table of nine is as follows: ");
         System.out.printf("%d\n",9);
         System.out.printf("%d\n",18);
         System.out.printf("%d\n",27);
@@ -10,7 +10,10 @@ public class PrintATableUsingPrintf {
         System.out.printf("%d\n",63);
         System.out.printf("%d\n",72);
         System.out.printf("%d\n",81);
-        System.out.printf("%d",90);
-
+        System.out.printf("%d",90);*/
+        int mulFac;
+        for(mulFac=1;mulFac<=10;mulFac++){
+         System.out.println("9 *"+mulFac+"= "+ (9*mulFac));
+        }
     }
 }
