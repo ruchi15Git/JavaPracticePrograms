d7e61de7a35e9391cc21932f4563128465e0796e
Anjali <anjuchate15@gmail.com>
2025-01-22 10:46:21 +0530

    increament

--- src/IncreamentAndDecreament/IncDec001.java
+++ src/IncreamentAndDecreament/IncDec001.java
@@ -2,15 +2,8 @@ package IncreamentAndDecreament;
 
 public class IncDec001 {
     public static void main(String[] args){
-      /*  int a=10;
-        System.out.println(a++ + ++a);
-       */
-        /*int a = 10;
-        System.out.println(++a + a++ + a++); //11+11+12=34
-        System.out.println(a);//13
-         */
         int a= 20;
-        System.out.println(--a + a++ + a--); //19 +19+20=58
+        System.out.println(--a + a++ + a--); //19+19+20=58
 
     }
 }
