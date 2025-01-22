18427eff524baf83c508445fa1fb8a952aa0bb34
Anjali <anjuchate15@gmail.com>
2025-01-22 10:44:18 +0530

    increament

--- src/IncreamentAndDecreament/IncreamentAdvancedProb.java
+++ src/IncreamentAndDecreament/IncreamentAdvancedProb.java
@@ -2,10 +2,8 @@ package IncreamentAndDecreament;
 
 public class IncreamentAdvancedProb {
     public static void main(String[] args){
-        int a=10;
-        System.out.println(a++ + a);
-        //A+B
-        //A=a++->a=10
-        //B=a ->11
+        int a = 10;
+        System.out.println(++a + a++ + a++); //11+11+12=34
+        System.out.println(a); //13
     }
 }
