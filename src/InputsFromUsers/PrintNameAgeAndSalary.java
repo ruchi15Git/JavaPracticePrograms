a5b919843ca73ac3ee2c350d65f145d311236a9e
Anjali <anjuchate15@gmail.com>
2025-01-22 10:41:33 +0530

    Pushing CLI problems

---
+++ src/InputsFromUsers/PrintNameAgeAndSalary.java
@@ -0,0 +1,12 @@
+package InputsFromUsers;
+
+public class PrintNameAgeAndSalary {
+    public static void main(String[] args){
+        String name=args[0];
+        int age=Integer.parseInt(args[1]);
+        int salary=Integer.parseInt(args[2]);
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Salary: "+salary);
+    }
+}
---
+++ src/InputsFromUsers/TakeInputFromCLIAndFindMaxNum.java
@@ -0,0 +1,10 @@
+package InputsFromUsers;
+
+public class TakeInputFromCLIAndFindMaxNum {
+    public static void main(String[] args){
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        int result=(a>b)?a:b;
+        System.out.println("Max number: "+result);
+    }
+}
